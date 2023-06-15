09b6aa3143dee3a093b78f7b0d171074627fd462
Holly Summers <kloe4emaeil+git@gmail.com>
2023-06-15 11:31:55 +0200

    Initial Commit

---
+++ App-Wecker-v4/App_Wecker_v4App.swift
@@ -0,0 +1,17 @@
+//
+//  App_Wecker_v4App.swift
+//  App-Wecker-v4
+//
+//  Created by Enno Nussbaum on 15.06.23.
+//
+
+import SwiftUI
+
+@main
+struct App_Wecker_v4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
